796a0cf9e0b7642e8455ca6582c70780bcba1946
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-18 13:41:03 -0300

    prepare custom home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,12 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <h1>Home page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'HomeView',
+export default {
 
-    components: {
-      HelloWorld,
-    },
-  }
+}
 </script>
