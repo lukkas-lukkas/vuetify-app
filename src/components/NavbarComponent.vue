e4a9fe7cc74eb6c33c685ef7133e2d8c6bfcfd8a
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-18 16:42:13 -0300

    create itens in sidebar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>        
+    <nav>
         <v-toolbar flat app>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
@@ -16,7 +16,18 @@
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" app class="primary" temporary>
-            <p>Test</p>
+            <v-list>
+                <v-list-item-group>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-icon>
+                            <v-icon class="white--text">{{ link.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -25,7 +36,12 @@
 export default {
     data() {
         return {
-            drawer: false,
+            drawer: true,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+                { icon: 'mdi-account-circle', text: 'Team', route: '/team' },
+            ]
         }
     }
 }
