2976e3c222705b7cb44fdb0d8f54b28bd8a4bca3
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-18 18:45:55 -0300

    create first task on dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,7 +3,26 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container fluid class="my-5">
-      content
+      <v-card flat>
+        <v-layout row wrap class="grey lighten-4 pa-5">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>Manage the Scranton branch</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>Michael Scott</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 1990</div>
+          </v-flex>
+          <v-flex xs4 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>Pending</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
