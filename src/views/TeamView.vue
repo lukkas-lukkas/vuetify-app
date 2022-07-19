b222fce2794690522774c8c9d6cf53645cbec1b8
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-19 15:58:58 -0300

    create team cards

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -3,7 +3,40 @@
         <h1 class="subheading grey--text">Team</h1>
 
         <v-container class="my-5">
-            content
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                    <v-card flat class="text-xs-center ma-3">
+                        <v-responsive class="pt-4">
+                            image here
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="subheading">{{ person.name }}</div>
+                            <div class="grey--text">{{ person.role }}</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn text color="grey darken-1">
+                                <v-icon small left>mdi-message</v-icon>
+                                <span>Message</span>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
         </v-container>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            team: [
+                { name: 'Michael Scott', role: 'Regional Manager' },
+                { name: 'Jim Halpert', role: 'Seller' },
+                { name: 'Dwight Schrute', role: 'Regional assistant manager' },
+                { name: 'Pam Beesly', role: 'Receptionist' }
+            ]
+        }
+    }
+}
+</script>
