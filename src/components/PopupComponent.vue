2abf826baa010d44ac7428b6f4b19426bd43eca0
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-19 23:38:25 -0300

    create form in popup

--- src/components/PopupComponent.vue
+++ src/components/PopupComponent.vue
@@ -3,14 +3,19 @@
         <template v-slot:activator="{ on, attrs }">
             <v-btn color="success" v-bind="attrs" v-on="on">Add new project</v-btn>
         </template>
-        <template v-slot:default="dialog">
+        <template>
             <v-card>
-                <v-toolbar color="primary" dark>Opening from the bottom</v-toolbar>
+                <v-toolbar color="primary" dark>Add new project form</v-toolbar>
                 <v-card-text>
-                    <div class="text-h2 pa-12">Hello world!</div>
+                    <v-form class="px-3">
+                        <v-text-field v-model="title" label="Title" prepend-icon="mdi-folder">
+                        </v-text-field>
+                        <v-textarea v-model="description" label="Information" prepend-icon="mdi-pencil">
+                        </v-textarea>
+                    </v-form>
                 </v-card-text>
-                <v-card-actions class="justify-end">
-                    <v-btn text @click="dialog.value = false">Close</v-btn>
+                <v-card-actions class="justify-end pb-5">
+                    <v-btn text @click="submit" class="success mx-0 mt-3">Add project</v-btn>
                 </v-card-actions>
             </v-card>
         </template>
@@ -19,6 +24,17 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            title: '',
+            description: ''
+        }
+    },
+    methods: {
+        submit() {
+            console.log('Title', this.title);
+            console.log('Description', this.description);
+        }
+    }
 }
 </script>
