b25d37ff1743273fb13fa946c167b82bcaf04ca0
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-19 21:57:02 -0300

    add expansion panels in projects view

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,7 +3,49 @@
     <h1 class="subheading grey--text">Projects</h1>
 
     <v-container class="my-5">
-      content
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project in projects" :key="project.title">
+          <v-expansion-panel-header>
+            {{ project.title }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="px-4 grey--text">
+            <div class="font-weight-bold">due by {{ project.due }}</div>
+            <div>{{ project.description }}</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: 'Manage the Scranton branch',
+          person: 'Michael Scott',
+          due: '1st Jan 1990',
+          status: 'pending',
+          description: 'Description of the project'
+        },
+        {
+          title: 'Marry with Pam',
+          person: 'Jim Halpert',
+          due: '1st Sep 2009',
+          status: 'done',
+          description: 'Description of the project'
+        },
+        {
+          title: 'Eat pizza',
+          person: 'Kevin Malone',
+          due: '1st Sep 2010',
+          status: 'ongoing',
+          description: 'Description of the project'
+        },
+      ]
+    }
+  }
+}
+</script>
